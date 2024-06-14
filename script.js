204fac12eb7d32147665db5add17371b47fab34b
Luiz Fernando C. A Filho <108531604+luizkujo@users.noreply.github.com>
2024-06-14 11:31:55 -0300

    ediit

---
+++ script.js
@@ -0,0 +1,22 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resulato");
+const textoResultado= document.querySelector(".texto-resultado");
+const = perguntas [
+    {
+    enunciado:"Pergunta 1",
+    alternativas:[
+        "Alternativa 1",
+        "Alternativas 2",
+    ]
+
+}
+{
+    enunciado:"Pergunta 2",
+    alternativas:[
+        "Alternativa 1",
+        "Alternativas 2",
+    ]
+
+}
